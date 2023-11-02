6900ba9726d36a89c1ba938cfae566400026b1ef
chLi <565353780@qq.com>
2023-11-02 12:11:28 +0800

    remove registry module

--- j_nerf/Config/registry.py
+++
@@ -1,9 +0,0 @@
-from j_nerf.Data.registry import Registry
-
-DATASETS = Registry()
-ENCODERS = Registry()
-NETWORKS = Registry()
-SAMPLERS = Registry()
-LOSSES = Registry()
-OPTIMS = Registry()
-SCHEDULERS = Registry()
